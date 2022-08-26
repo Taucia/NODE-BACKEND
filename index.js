ec2f5b177f03f9b4d34ed17f6360e0a590e09b56
taucia <taucia.jackson123@gmail.com>
2022-08-26 16:01:11 +0200

    Updates

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const router = express.Router();
 const port = parseInt(process.env.PORT) || 4000;
 const path = require('path');
 
-// app.use(cors({
-//     mode: 'no-cors',
-//     origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://192.168.9.62:8080/'],
-//     credentials: true
-// }));
 // allow access to fetch data from the api externally by  Seting header
 app.use((req, res, next) => {
     res.setHeader("mode", "no-cors");
@@ -26,6 +21,12 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-*", "*");
     next();
 });
+
+app.use(cors({
+    mode: 'no-cors',
+    origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://192.168.9.62:8080/'],
+    credentials: true
+}));
 // add cors to the app variable
 app.use(
     router,
