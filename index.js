17b3b6302f700722aa8688d92fc7d3c022a15966
taucia <taucia.jackson123@gmail.com>
2022-08-26 14:57:59 +0200

    Sorting out CORS

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = parseInt(process.env.PORT) || 4000;
 const path = require('path');
 
 app.use(cors({
-    origin: ['http://127.0.0.1:8080', 'http://localhost:8080'],
+    origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://192.168.9.62:8080/'],
     credentials: true
 }));
 // add cors to the app variable
